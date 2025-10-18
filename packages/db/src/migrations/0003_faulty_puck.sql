4ad338074cffb45d3baee78cc8bbd51bc28b746e
Siddique Khan <project.purpose.rollno.29@gmail.com>
2025-10-18 21:07:09 +0530

    removed old migrations

--- packages/db/src/migrations/0003_faulty_puck.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "medical_record" DROP CONSTRAINT "medical_record_patient_id_user_id_fk";
---> statement-breakpoint
-ALTER TABLE "medical_record" DROP CONSTRAINT "medical_record_clinician_id_user_id_fk";
---> statement-breakpoint
-ALTER TABLE "medical_record" ADD CONSTRAINT "medical_record_patient_id_user_id_fk" FOREIGN KEY ("patient_id") REFERENCES "public"."user"("id") ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "medical_record" ADD CONSTRAINT "medical_record_clinician_id_user_id_fk" FOREIGN KEY ("clinician_id") REFERENCES "public"."user"("id") ON DELETE restrict ON UPDATE no action;
